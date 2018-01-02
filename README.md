e614c2a1ddc87e4504764ce18d57d6a013138159
Victor Suarez <suarez.garcia.victor@gmail.com>
2018-01-02 23:53:04 +0100

    Se ha creado un nuevo bot para reproducir musica via youtube.

--- README.md
+++ README.md
@@ -13,3 +13,19 @@ $ pip install -U discord.py
 Para poder usar el bot se requiere un token que deberá solicitarse como se indica [aquí](https://github.com/reactiflux/discord-irc/wiki/Creating-a-discord-bot-&-getting-a-token).
 El token deberá ponerse en el fichero ```settings.py```.
 
+---
+
+## Cueh Music Bot
+
+Segundo Discord Bot (cuehmusicbot.py) que reproduce musica desde Youtube. Requiere la libreria ```youtube-dl``` la cual puede instalarse con Pip 
+```
+$ pip install youtube-dl
+```
+
+Además requiere de la librería _libOpus_ que debe instalarse en su sistema Operativo.
+
+El bot funciona de la siguiente manera. Primero debe conectarse al canal de voz donde se encuentre quien quiera escuchar con el comando ```!cueh```.
+
+Una vez conectado ejecutar el comando _!cuehplay_ seguido de la dirección de youtube a reproducir.
+
+Si se quiere parar su reproducción, ejecutar el comando !cuehstop o poner otra canción.
---
+++ cuehmusicbot.py
@@ -0,0 +1,56 @@
+import discord
+import settings
+from discord.ext import commands
+
+discord.opus.load_opus('libopus.0.dylib')
+
+bot = commands.Bot(command_prefix=commands.when_mentioned_or('!'), description='Reproduce musica desde youtube')
+
+class Cueh:
+    def __init__(self, bot):
+        self.bot = bot
+        self.voice = None
+        self.player = None
+
+    async def join_channel(self,voicechannel):
+        self.voice = await self.bot.join_voice_channel(voicechannel)
+        pass
+    @commands.command(pass_context=True, no_pm=True)
+    async def cueh(self, ctx):
+        voice_channel = ctx.message.author.voice_channel
+        print('Conectando a {}'.format(voice_channel))
+        if voice_channel is None:
+            await self.show_message('Primero Conectate a un canal de voz, Cueh', mtts=True)
+        await self.join_channel(voice_channel)
+
+    @commands.command(pass_context=True, no_pm=True)
+    async def cuehplay(self, ctx, urlsong):
+        if self.player != None:
+            if not self.player.is_done():
+                await self.show_message("Parando {}".format(self.player.title), mtts=True)
+                self.player.stop()
+        print("Reproduciendo {}".format(urlsong))
+        self.player = await self.voice.create_ytdl_player(urlsong)
+        msg = "Reproduciendo {}, cueh".format(self.player.title)
+        await self.show_message(msg, mtts=True)
+        self.player.start()
+        pass
+
+    @commands.command(pass_context=True, no_pm=True)
+    async def cuehstop(self, ctx):
+        if self.player != None:
+            if not self.player.is_done():
+                msg = "Parando {}, cueh".format(self.player.title)
+                await self.show_message(msg, mtts=True)
+                self.player.stop()
+        pass
+    async def show_message(self, msg, mtts=False):
+        print(msg)
+        await self.bot.say(msg, tts=mtts)
+        pass
+bot.add_cog(Cueh(bot))
+@bot.event
+async def on_ready():
+    print("cueh v{} ready".format(settings.version))
+    print("Logged into {} ".format(bot.user.id))
+bot.run(settings.bot_token)
